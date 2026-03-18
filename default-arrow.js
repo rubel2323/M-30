d0f6d13adef323b99e9cf7bebfc3012db4c0923c
Rubel <mr.adcadre41@gmail.com>
2026-03-18 11:36:25 +0600

    array by arrow added

---
+++ default-arrow.js
@@ -0,0 +1,6 @@
+const addition = (num1, num2 = 2) => {
+  const makeaddition = num1 + num2;
+  return makeaddition;
+};
+const result = addition(12);
+console.log(result);
---
+++ practice-problem-2.js
@@ -0,0 +1,12 @@
+const showFriend = (names) => {
+  friendEven = [];
+  console.log(names);
+  for (const name of names) {
+    if (name.length % 2 !== 1) {
+      friendEven.push(name);
+    }
+  }
+  return friendEven;
+};
+const friends = ["bari", "mariyo", "mokbulo", "shikha"];
+console.log(showFriend(friends));
